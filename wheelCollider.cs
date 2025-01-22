53d7c28c75edf80c32c21e448ecd5b9d3f47cc28
Mikkel Neiiendam <mikklelamstrup@gmail.com>
2025-01-22 10:25:11 +0100

    tilføjet wheel collider

---
+++ wheelCollider.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentBrakeForce;
+    private bool isBreaking;
+
+    // Indstillinger
+    [SerializeField] private float motorForce = 1500f;
+    [SerializeField] private float brakeForce = 3000f;
+    [SerializeField] private float maxSteerAngle = 30f;
+
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    // Wheel Transforms
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    private void FixedUpdate()
+    {
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    public void SetInputs(float moveInput, float steeringInput)
+    {
+        verticalInput = moveInput;       // Acceleration og bremsning
+        horizontalInput = steeringInput; // Styring
+    }
+
+    private void HandleMotor()
+    {
+        // Acceleration
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        // Bremsning
+        currentBrakeForce = verticalInput == 0 ? brakeForce : 0f;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        // Tilføj bremsekraft til alle hjul
+        frontRightWheelCollider.brakeTorque = currentBrakeForce;
+        frontLeftWheelCollider.brakeTorque = currentBrakeForce;
+        rearLeftWheelCollider.brakeTorque = currentBrakeForce;
+        rearRightWheelCollider.brakeTorque = currentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        // Styring baseret på input
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        // Opdater hjulposition og rotation
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+}
